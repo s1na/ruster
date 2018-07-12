262b434d4b3d6098b22ecd98206bd924c6b68244
Sina Mahmoodi <itz.s1na@gmail.com>
2018-07-12 21:16:27 +0200

    Add wasmi example, deps
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate wabt;
+extern crate wasmi;
+
+use wasmi::{ImportsBuilder, ModuleInstance, NopExternals, RuntimeValue};
+
 fn main() {
-    println!("Hello, world!");
+    let wasm_binary: Vec<u8> = wabt::wat2wasm(
+        r#"
+            (module
+                (func (export "test") (result i32)
+                    i32.const 1337
+                )
+            )
+            "#,
+    ).expect("failed to parse wat");
+
+    let module = wasmi::Module::from_buffer(&wasm_binary).expect("failed to load wasm");
+
+    let instance = ModuleInstance::new(&module, &ImportsBuilder::default())
+        .expect("failed to instantiate wasm module")
+        .assert_no_start();
+
+    assert_eq!(
+        instance
+            .invoke_export("test", &[], &mut NopExternals)
+            .expect("failed to execute export"),
+        Some(RuntimeValue::I32(1337)),
+    );
 }
