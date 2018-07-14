bb0d5184a7bb0df10eac0e798af08f5707b6415a
Sina Mahmoodi <itz.s1na@gmail.com>
2018-07-14 20:56:36 +0200

    Divide example into two fns
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
 extern crate wabt;
 extern crate wasmi;
 
-use wasmi::{ImportsBuilder, ModuleInstance, NopExternals, RuntimeValue};
+use wasmi::{ImportsBuilder, ModuleInstance, ModuleRef, NopExternals, RuntimeValue};
 
 fn main() {
+    let module = new_module();
+    let res = add(module, 3, 5);
+
+    println!("Result of add: {:?}", res);
+}
+
+fn add(instance: ModuleRef, a: i32, b: i32) -> Result<i32, &'static str> {
+    let params = [RuntimeValue::I32(a), RuntimeValue::I32(b)];
+
+    match instance.invoke_export("add", &params, &mut NopExternals) {
+        Ok(r) => match r {
+            Some(runtime_value) => match runtime_value {
+                RuntimeValue::I32(v) => Ok(v),
+                _ => Err("invalid type"),
+            },
+            None => Err("emtpy result"),
+        },
+        Err(_) => Err("wasmi error"),
+    }
+}
+
+fn new_module() -> ModuleRef {
     let wasm_binary: Vec<u8> = wabt::wat2wasm(
         r#"
             (module
-                (func (export "test") (result i32)
-                    i32.const 1337
-                )
-            )
-            "#,
+                (func $add (param i32 i32) (result i32)
+                    get_local 0
+                    get_local 1
+                    i32.add)
+                (export "add" (func $add)))
+         "#,
     ).expect("failed to parse wat");
 
     let module = wasmi::Module::from_buffer(&wasm_binary).expect("failed to load wasm");
 
-    let instance = ModuleInstance::new(&module, &ImportsBuilder::default())
+    ModuleInstance::new(&module, &ImportsBuilder::default())
         .expect("failed to instantiate wasm module")
-        .assert_no_start();
-
-    assert_eq!(
-        instance
-            .invoke_export("test", &[], &mut NopExternals)
-            .expect("failed to execute export"),
-        Some(RuntimeValue::I32(1337)),
-    );
+        .assert_no_start()
 }
