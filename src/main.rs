4044a61ee7f91074708de86c1eaacb3232ab7471
Sina Mahmoodi <itz.s1na@gmail.com>
2018-07-26 16:37:50 +0200

    Move all code from main to lib, only invoke run in main
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-extern crate codegen;
-extern crate parity_wasm;
-extern crate wabt;
-extern crate wasmi;
+extern crate ruster;
 
-mod generate;
-mod extract;
-
-use self::generate::generate_rust;
-use self::extract::{get_exported_fns};
 use std::env;
-use wasmi::{ImportsBuilder, ModuleInstance, ModuleRef, NopExternals, RuntimeValue};
+use std::process;
+
+use ruster::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,36 +11,8 @@ fn main() {
         return;
     }
 
-    let module = parity_wasm::deserialize_file(&args[1]).expect("File to be deserialized");
-    let module = module.parse_names().expect("Names to be parsed");
-    let exported_fns = get_exported_fns(&module);
-    println!("Exported functions {:?}", exported_fns);
-    generate_rust(exported_fns);
-
-    let module = new_module(module);
-    let res = add(module, 3, 5);
-    println!("Result of add: {:?}", res);
-}
-
-fn add(instance: ModuleRef, a: i32, b: i32) -> Result<i32, &'static str> {
-    let params = [RuntimeValue::I32(a), RuntimeValue::I32(b)];
-
-    match instance.invoke_export("add", &params, &mut NopExternals) {
-        Ok(r) => match r {
-            Some(runtime_value) => match runtime_value {
-                RuntimeValue::I32(v) => Ok(v),
-                _ => Err("invalid type"),
-            },
-            None => Err("emtpy result"),
-        },
-        Err(_) => Err("wasmi error"),
+    if let Err(e) = run(&args[1]) {
+        println!("error: {}", e);
+        process::exit(1);
     }
-}
-
-fn new_module(module: parity_wasm::elements::Module) -> ModuleRef {
-    let module = wasmi::Module::from_parity_wasm_module(module).expect("failed to load wasm");
-
-    ModuleInstance::new(&module, &ImportsBuilder::default())
-        .expect("failed to instantiate wasm module")
-        .assert_no_start()
 }
