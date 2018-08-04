3552cd17ae7303586b864905258151270cb53cb6
Sina Mahmoodi <itz.s1na@gmail.com>
2018-08-04 16:50:26 +0200

    Add clap, accept output as argument
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
+extern crate clap;
 extern crate ruster;
 
-use std::env;
+use clap::{App, Arg};
 use std::process;
 
 use ruster::run;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("please provide the path to the wasm module");
-        return;
-    }
+    let matches = App::new("ruster")
+        .version("0.1.0")
+        .about("Convert wasm to importable rust modules")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Path to wasm file")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .help("Path to the resulting rust module")
+                .takes_value(true),
+        )
+        .get_matches();
 
-    if let Err(e) = run(&args[1], "tests/out.rs") {
+    let input = matches.value_of("INPUT").unwrap();
+    let output = matches.value_of("output").unwrap_or("out.rs");
+
+    if let Err(e) = run(input, output) {
         eprintln!("error: {}", e);
         process::exit(1);
     }
